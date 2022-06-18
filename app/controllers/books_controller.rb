7f53e31e3521c10d5bb27959f654cc2c65edd9fa
gyooooo <harapeko2925@gmail.com>
2022-06-18 06:28:41 +0000

    fullygit add .git add .

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,13 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
-   if @book.save
+    if @book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
-   else
+    else
     @books = Book.all
     render :index
-   end
+    end
   end
 
   def index
@@ -27,9 +28,30 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   
+  def destroy
+     @book = Book.find(params[:id])
+     if @book.destroy
+     flash[:notice] = "Book was successfully destroyed."
+     redirect_to books_path
+    else
+    @books = Book.all
+    render :index
+    end
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+    else
+    render :edit
+    end
+  end
+  
+  
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
 end
-
